11f75921a9d5afbb5118874e8e5c33af567f78b2
Kacper Kawecki <kacper@geniebelt.com>
2016-05-03 01:17:43 +0200

    Added more specs

--- spec/base/store_spec.rb
+++ spec/base/store_spec.rb
@@ -23,4 +23,24 @@ describe 'Base storing' do
     expect(foo.persisted?).to eq true
     expect(foo.new_record?).to eq false
   end
+
+  it 'should create object' do
+    foo = Foo.create bar: :test
+    expect(foo.id).to be
+    expect(foo.persisted?).to eq true
+    expect(foo.new_record?).to eq false
+  end
+
+  it 'should return false while saving not valid' do
+    Foo.class_eval do
+      attributes :x
+      validates :x, presence: true
+    end
+    foo = Foo.new bar: :test
+    expect(foo.new_record?).to be_truthy
+    expect(foo.save).to be_falsey
+    expect(foo.id).to be_nil
+    expect(foo.persisted?).to eq false
+    expect(foo.new_record?).to eq true
+  end
 end
