55d0aef02fcb63b441b445bdf7e0e9a102d5631d
Kacper Kawecki <kacper@geniebelt.com>
2016-06-09 19:33:39 +0200

    Added test for polymorphic belongs_to relations

--- spec/associations/belongs_to_spec.rb
+++ spec/associations/belongs_to_spec.rb
@@ -68,4 +68,53 @@ describe 'belongs_to association' do
     expect(foo.bar).to eq bar2
   end
 
+  context 'polymorphic' do
+    it 'should define with just name' do
+      undefine_class :Foo
+      expect do
+        class Foo < MemoryRecord::Base;
+          attributes id: Integer, item_id: Integer, item_type: String
+          belongs_to :item, polymorphic: true
+        end
+      end.not_to raise_error
+    end
+
+    it 'should set proper values' do
+      undefine_class :Foo
+      undefine_class :Bar
+      class Bar < MemoryRecord::Base
+        attributes id: Integer, name: String
+      end
+      class Foo < MemoryRecord::Base;
+        attributes id: Integer, item_id: Integer, item_type: String
+        belongs_to :item, polymorphic: true
+      end
+
+      bar = Bar.create! name: 'test'
+      foo = Foo.create! item: bar
+      expect(foo.item_id).to eq bar.id
+      expect(foo.item_type).to eq 'Bar'
+    end
+
+    it 'should load proper values' do
+      undefine_class :Foo
+      undefine_class :Bar
+      class Bar < MemoryRecord::Base
+        attributes id: Integer, name: String
+      end
+      class Foo < MemoryRecord::Base;
+        attributes id: Integer, item_id: Integer, item_type: String
+        belongs_to :item, polymorphic: true
+      end
+      bar = Bar.create! name: 'test'
+      bar2 = Bar.create! name: 'test 2'
+      Bar.create! name: 'test 3'
+      foo = Foo.create! item_id: bar.id, item_type: 'Bar'
+      expect(foo.item_id).to eq bar.id
+      expect(foo.item).to eq bar
+
+      foo.item_id = bar2.id
+      expect(foo.item).to eq bar2
+    end
+  end
 end
